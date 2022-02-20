873abd9fae123f9bc69086c8c57d2c09b9c3531f
chaitu25 <chaitanyajoglekar2@gmail.com>
2022-02-20 23:50:31 +0000

    Adding yml parsers from rasa

--- utils/training_data_augmentor.py
+++ utils/training_data_augmentor.py
@@ -1,7 +1,12 @@
 # from rasa.shared.nlu.training_data.message import Message
-# from rasa.shared.nlu.training_data.training_data import TrainingData
+from rasa.shared.nlu.training_data.training_data import TrainingData
 # from rasa.shared.nlu.training_data.training_data
 from rasa.shared.nlu.training_data.formats.rasa_yaml import RasaYAMLReader
-filepresent = RasaYAMLReader.is_yaml_nlu_file("./data/nlu.yml")
-if(RasaYAMLReader):
+filepresent = RasaYAMLReader.is_yaml_nlu_file("../data/nlu.yml")
+if(filepresent):
     print('Yes file is present')
+    ymlReader = RasaYAMLReader()
+    data = TrainingData()
+    data = ymlReader.read("../data/nlu.yml")
+    print('printing file')
+    print(data.intents)
