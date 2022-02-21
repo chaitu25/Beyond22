f740d9be68a1f1b65f852e145cd802688c3c4ec1
Chaitanya Joglekar <chaitanyajoglekar2@gmail.com>
2022-02-21 10:55:33 +0000

    Adding training data to be expanded

--- utils/training_data_augmentor.py
+++ utils/training_data_augmentor.py
@@ -1,7 +1,10 @@
-# from rasa.shared.nlu.training_data.message import Message
+from rasa.shared.nlu.training_data.message import Message
 from rasa.shared.nlu.training_data.training_data import TrainingData
 # from rasa.shared.nlu.training_data.training_data
 from rasa.shared.nlu.training_data.formats.rasa_yaml import RasaYAMLReader
+import os
+import argparse
+
 filepresent = RasaYAMLReader.is_yaml_nlu_file("../data/nlu.yml")
 if(filepresent):
     print('Yes file is present')
@@ -10,3 +13,24 @@ if(filepresent):
     data = ymlReader.read("../data/nlu.yml")
     print('printing file')
     print(data.intents)
+
+
+
+if __name__ == "__main__":
+    filename = ''
+    traingData = dict()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fileName',type=str)
+    args = parser.parse_args()
+    if args.fileName is None:
+        filename = 'trainingData.txt'
+    filename = args.fileName
+    with open(filename,'r') as file:
+        data = file.readline().split(' ')
+        intent = data[0]
+        utterance = data[1]
+        traingData[utterance] = intent
+    
+    print('printing value from dict')
+    for k,v in traingData.items():
+        print(k,'->>>',v)
